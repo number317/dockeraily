4fb3581882dc1c1d2ac910dd2496c3679ea794cf
cheon <cheon0112358d@gmail.com>
2018-12-29 14:49:44 +0800

    [FIX]fix db path

--- kafka/kafka-eagle/entrypoint.sh
+++ kafka/kafka-eagle/entrypoint.sh
@@ -2,5 +2,6 @@
 
 sed -i "s/ZKSERVER/${ZKSERVER}/g" /kafka-eagle/conf/system-config.properties
 chmod +x /kafka-eagle/bin/ke.sh
+mkdir -p /kafka-eagle/db/
 sh /kafka-eagle/bin/ke.sh start
 tail -f /kafka-eagle/logs/log.log
